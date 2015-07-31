c9ba45c5772615d814368fc4741b7283add81b28
Lindsey DiLoreto <lindsey@doublesecretagency.com>
2015-07-31 13:10:08 -0700

    Replaced "welcome" page

--- adwizard/AdWizardPlugin.php
+++ adwizard/AdWizardPlugin.php
@@ -4,48 +4,47 @@ namespace Craft;
 class AdWizardPlugin extends BasePlugin
 {
 
-    public function getName()
-    {
-        return Craft::t('Ad Wizard');
-    }
+	public function getName()
+	{
+		return Craft::t('Ad Wizard');
+	}
 
-    public function getVersion()
-    {
-        return '1.1.1';
-    }
+	public function getVersion()
+	{
+		return '1.1.1';
+	}
 
-    public function getDeveloper()
-    {
-        return 'Double Secret Agency';
-    }
+	public function getDeveloper()
+	{
+		return 'Double Secret Agency';
+	}
 
-    public function getDeveloperUrl()
-    {
-        return 'https://craftpl.us/plugins/ad-wizard';
-        //return 'http://doublesecretagency.com';
-    }
+	public function getDeveloperUrl()
+	{
+		return 'https://craftpl.us/plugins/ad-wizard';
+		//return 'http://doublesecretagency.com';
+	}
 
-    public function hasCpSection()
-    {
-        return true;
-    }
+	public function hasCpSection()
+	{
+		return true;
+	}
 
-    public function registerCpRoutes()
-    {
-        return array(
-            'adwizard'                                            => array('action' => 'adWizard/adIndex'),
-            'adwizard/positions'                                  => array('action' => 'adWizard/positionIndex'),
-            'adwizard/positions/new'                              => array('action' => 'adWizard/editPosition'),
-            'adwizard/positions/(?P<positionId>\d+)'              => array('action' => 'adWizard/editPosition'),
-            'adwizard/(?P<positionHandle>{handle})/new'           => array('action' => 'adWizard/editAd'),
-            'adwizard/(?P<positionHandle>{handle})/(?P<adId>\d+)' => array('action' => 'adWizard/editAd'),
-        );
-    }
+	public function registerCpRoutes()
+	{
+		return array(
+			'adwizard'                                            => array('action' => 'adWizard/adIndex'),
+			'adwizard/positions'                                  => array('action' => 'adWizard/positionIndex'),
+			'adwizard/positions/new'                              => array('action' => 'adWizard/editPosition'),
+			'adwizard/positions/(?P<positionId>\d+)'              => array('action' => 'adWizard/editPosition'),
+			'adwizard/(?P<positionHandle>{handle})/new'           => array('action' => 'adWizard/editAd'),
+			'adwizard/(?P<positionHandle>{handle})/(?P<adId>\d+)' => array('action' => 'adWizard/editAd'),
+		);
+	}
 
-    public function onAfterInstall()
-    {
-        // @TODO: Change to "Introduction" page
-        craft()->request->redirect(UrlHelper::getCpUrl('adwizard/thanks'));
-    }
-    
+	public function onAfterInstall()
+	{
+		craft()->request->redirect(UrlHelper::getCpUrl('adwizard/welcome'));
+	}
+
 }
