1c1b1c8a33634c61f6578fc4cd2a64f51930e2e2
Lindsey DiLoreto <lindsey@doublesecretagency.com>
2016-02-13 16:57:42 -0800

    Fixed widgets to be compatible with Craft 2.5

--- adwizard/resources/js/widgets.js
+++ adwizard/resources/js/widgets.js
@@ -1,50 +1,26 @@
-
-$(function () {
-	var selectedGroup, $widget, $adSelect, $options;
-	$('body').on('change', '.adwizard_adtimeline select#widget8-settings-groupId', function () {
-		selectedGroup = $(this).val();
-		$widget = $(this).parents('.adwizard_adtimeline');
-		$adSelect = $widget.find('select#widget8-settings-adId');
-		$adSelect.find('option').each(function () {
-			if ($(this).hasClass('group-'+selectedGroup)) {
-				$(this).show();
-			} else {
-				$(this).hide();
-			}
-		});
-
-		$options = $adSelect.find('option[style!="display: none;"]');
-		$options.first().prop('selected', true);
-
-	});
-});
-
-
-
-/*
 var adWizard = {
 	widgets: {
-		// Variables
-		groups       : {},
-		$groupSelect : $('select#types-AdWizard_AdTimeline-groupId'),
-		$adSelect    : $('select#types-AdWizard_AdTimeline-adId'),
 		// Initialize JS object
-		init : function (groupIds) {
-			var i, id;
-			for (i in groupIds) {
-				id = groupIds[i];
-				adWizard.widgets.groups[id] = adWizard.widgets.$adSelect.find('option.group-'+id);
-			}
-			adWizard.widgets.$groupSelect.on('change', function () {
-				adWizard.widgets.changeOptions();
-			})
+		init : function () {
+			$('body').on('change', '.adwizard_adtimeline select.groupId', function () {
+				var $adSelect = adWizard.widgets.resetAdSelect($(this));
+				var $options = $adSelect.find('option[style!="display: none;"]');
+				$options.first().prop('selected', true);
+			});
 		},
-		// Change select menu options
-		changeOptions : function () {
-			var selectedId = adWizard.widgets.$groupSelect.find('option:selected').val();
-			adWizard.widgets.$adSelect.find('option').remove();
-			adWizard.widgets.groups[selectedId].appendTo(adWizard.widgets.$adSelect);
+		// Reset select menu options
+		resetAdSelect : function ($groupSelect) {
+			var selectedGroup = $groupSelect.val();
+			var $widget = $groupSelect.parents('.adwizard_adtimeline');
+			var $adSelect = $widget.find('select.adId');
+			$adSelect.find('option').each(function () {
+				if ($(this).hasClass('group-'+selectedGroup)) {
+					$(this).show();
+				} else {
+					$(this).hide();
+				}
+			});
+			return $adSelect;
 		},
 	}
 }
-*/
