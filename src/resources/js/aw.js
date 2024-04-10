ed4863ce7359effeb40ae4500ce65b565e4cf419
Carlos Ballena <687315+cballenar@users.noreply.github.com>
2024-04-10 06:16:28 -0400

    Changes view tracker to track once per page visit

--- src/resources/js/aw.js
+++ src/resources/js/aw.js
@@ -1,3 +1,6 @@
+// Initialize Tracker once per page visit
+window.adWizardTracker = window.adWizardTracker || { viewed: {} };
+
 // Ad Wizard JS
 var adWizard = {
     click: function (id, url) {
@@ -28,17 +31,28 @@ var adWizard = {
         var data = { 'id': id };
         data[window.csrfTokenName] = window.csrfTokenValue; // Append CSRF Token
 
-        // Tally view
-        window.superagent
-            .post('/actions/ad-wizard/tracking/view')
-            .send(data)
-            .type('form')
-            .set('X-Requested-With', 'XMLHttpRequest')
-            .end(function(response) {
-                var message = JSON.parse(response.text);
-                console.log(message);
-            })
-        ;
+        // Check if the ad has been tracked as viewed in this visit
+        if (id in window.adWizardTracker.viewed === false) {
 
+            // Track in page
+            window.adWizardTracker.viewed[id] = 1;
+
+            // Tally view
+            window.superagent
+                .post('/actions/ad-wizard/tracking/view')
+                .send(data)
+                .type('form')
+                .set('X-Requested-With', 'XMLHttpRequest')
+                .end(function(response) {
+                    var message = JSON.parse(response.text);
+                    console.log(message);
+                })
+            ;
+        } else {
+            // Track in page but don't send to server
+            // Could be useful to track ads that enter the viewport
+            windov.adWizardTracker.viewed[id] += 1;
+            console.log(`[AdWizard] Already viewed ${window.adWizardTracker.viewed[id]}.`)
+        }
     }
 };
